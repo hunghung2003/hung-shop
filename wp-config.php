1b46670033257d7c4f5a5438377bde134b5c80b1
hunghung2003 <140245769+hunghung2003@users.noreply.github.com>
2023-08-08 12:18:54 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -14,22 +14,22 @@
  * * ABSPATH
  *
  * @link https://wordpress.org/documentation/article/editing-wp-config-php/
- *
+ *mysql://bcbd1080aca795:e4592b78@us-cdbr-east-06.cleardb.net/heroku_3e8bd8353a9ea96?reconnect=true
  * @package WordPress
  */
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'heroku_3e8bd8353a9ea96' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'bcbd1080aca795' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'e4592b78' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'us-cdbr-east-06.cleardb.net' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
